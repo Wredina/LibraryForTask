7bf1aac4ef1b03b0d01ed4c14ae96990a2ee1d49
Sofiya <n.d.dead@mail.ru>
2023-03-07 14:09:41 +0300

    add task 70-71

---
+++ С#/task_70/Program.cs
@@ -0,0 +1,36 @@
+﻿// n-m+1
+// n = 15
+// m = 5
+// 15-5+1 = 11
+
+int[] GetRangeSum(int[] array, int m)
+{
+  int n = array.Length;
+  int[] t = new int[n - m + 1];
+
+  int index = 0;
+
+  for (int i = 0; i < m - n; i++)
+  {
+    for (int j = i; j <= i + m; j++)
+    {
+      t[index] += array[j];
+    }
+    index++;
+  }
+  return t;
+}
+
+int[] CreateArray(int size) => new int[size];
+
+string Print(int[] array) => $"[{String.Join(", ", array)}]";
+
+void Fill(ref int[] array) => array = array.Select(e => Random.Shared.Next(10)).ToArray();
+
+int[] numbers = CreateArray(15);
+Fill(ref numbers);
+System.Console.WriteLine(Print(numbers));
+
+int count = 5;
+int[] sumNum = GetRangeSum(numbers, count);
+System.Console.WriteLine(Print(sumNum));
---
+++ С#/task_71/Program.cs
@@ -0,0 +1,56 @@
+﻿string[] СreationArrString(int sizeArr)
+{
+ string[] array = new string[sizeArr];
+ string[] letters = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+ for (int j = 0; j < sizeArr; j++)
+ {
+  string word = "";
+  Random rnd = new Random();
+  int wordSize = rnd.Next(1, 10);
+  for (int i = 0; i < wordSize; i++)
+  {
+   word += letters[rnd.Next(0, 26)];
+  }
+  array[j] = word;
+ }
+ return array;
+}
+
+
+int SizeForMiniArray(string[] array)
+{
+ int sizeMiniArr = array.Length;
+ for (int i = 0; i < array.Length; i++)
+ {
+  if (array[i].Length > 3) sizeMiniArr -= 1;
+ }
+ return sizeMiniArr;
+}
+
+string[] CreateNewMiniArrayString(int size, string[] BigArr)
+{
+ int j = 0;
+ string[] miniArr = new string[size];
+ for (int i = 0; i < BigArr.Length; i++)
+ {
+  if (BigArr[i].Length <= 3)
+  {
+   miniArr[j] = BigArr[i];
+   j++;
+  }
+ }
+ return miniArr;
+}
+
+
+string[] arr = СreationArrString(new Random().Next(4, 8));
+Console.WriteLine(String.Join(", ", arr));
+int sizeMiniArray = SizeForMiniArray(arr);
+if (sizeMiniArray == 0) System.Console.WriteLine("нет элементов менее 4-х символов");
+else
+{
+ string[] miniArr = CreateNewMiniArrayString(sizeMiniArray, arr);
+ Console.WriteLine(String.Join(", ", miniArr));
+}
+
+

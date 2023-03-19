bf78329f19f03e8d405ee072bd07335f9ba3d9aa
Sofiya <n.d.dead@mail.ru>
2023-03-19 14:56:20 +0300

    new add Help_python - collections

--- Python/task_08/hom_wrk.py
+++ Python/task_08/hom_wrk.py
@@ -5,7 +5,7 @@ widthСhoco = int(input("введите ширину шоколадки: "))
 delChoco = int(input("сколько долек хотите отломить? "))
 
 sizeChoco = lenghtСhoco*widthСhoco
-if sizeChoco % 2 == 0:
+if (sizeChoco - delChoco) % 2 == 0:
     print("можно произвести один разлом шоколатки")
 else:
     print("нельзя произвести один разлом шоколадки")

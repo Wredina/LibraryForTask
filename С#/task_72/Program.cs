9e3aa5f53b24cd3b634bfc749da0bddb6ab56c38
Sofiya <n.d.dead@mail.ru>
2023-03-31 17:57:17 +0300

    new info for help_pyton

---
+++ С#/task_72/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("введите сколько киллометров проезжает машина в день:");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите сколько киллометров машина должна проехать: ");
+int s = Convert.ToInt32(Console.ReadLine());
+
+int day = (s + (n - 1)) / n;
+System.Console.WriteLine($"{day} - столько дней будет ехать машина");
---
+++ С#/task_73/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("введите трёхзначное число:");
+
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum > 999 || userNum < 100)
+{
+  System.Console.WriteLine("вы ввели не верное число");
+}
+else
+{
+  int firstNum = userNum / 100;
+  int twoNum = (userNum / 10) % 10;
+  int lastNum = userNum % 10;
+  int sumNum = firstNum + twoNum + lastNum;
+  System.Console.WriteLine(sumNum);
+}
